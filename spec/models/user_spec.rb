48eda4877067b0e86178bbcaabf30783818ab47e
Fyodor Parmanchukov <rezerbit@gmail.com>
2013-09-23 12:52:34 +0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -28,6 +29,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  #Добавить методы по такому же шаблону
+
 
   it { should be_valid }
 
@@ -128,4 +133,8 @@ describe User do
     end
   end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
